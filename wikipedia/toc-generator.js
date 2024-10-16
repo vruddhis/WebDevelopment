153713e009503dd421f47a834e1714977d0f0c51
Vruddhi Shah <vruddhishah173@gmail.com>
2024-10-16 16:33:46 +0530

    h3 in table of contents

--- wikipedia/toc-generator.js
+++ wikipedia/toc-generator.js
@@ -1,8 +1,5 @@
-
-
 document.addEventListener("DOMContentLoaded", function() {
     const mainContent = document.getElementById("main-content");
-    
     const tocList = document.getElementById("toc-list");
 
     const headings = mainContent.querySelectorAll("h2, h3");
@@ -15,11 +12,19 @@ document.addEventListener("DOMContentLoaded", function() {
         const listItem = document.createElement("li");
         const link = document.createElement("a");
         link.href = `#${heading.id}`; 
-        link.className = "style1"; 
         link.textContent = heading.textContent; 
-        listItem.appendChild(link);
 
-       
+        if (heading.tagName === "H2") {
+            link.style.fontSize = "1.4em"; 
+            link.style.fontWeight = "bold"; 
+            listItem.style.marginLeft = "1"; 
+        } else if (heading.tagName === "H3") {
+            link.style.fontSize = "1em"; 
+            link.style.fontWeight = "normal"; 
+            listItem.style.marginLeft = "20px"; 
+        }
+
+        listItem.appendChild(link);
         tocList.appendChild(listItem);
     });
 });
